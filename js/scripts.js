8e89efa6635ac811c4156e5eb2bfcf867322012a
Saijinkun <95833548+Saijinkun@users.noreply.github.com>
2023-09-21 01:54:58 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+// /*!
+//  * Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
+//  * Copyright 2013-2023 Start Bootstrap
+//  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
+//  */
+// // This file is intentionally blank
+// // Use this file to add JavaScript to your project
+
+// const typed = select('.typed');
+// if (typed) {
+// 	let typed_strings = typed.getAttribute('data-typed-items');
+// 	typed_strings = typed_strings.split(',');
+// 	new Typed('.typed', {
+// 		strings: typed_strings,
+// 		loop: true,
+// 		typeSpeed: 100,
+// 		backSpeed: 50,
+// 		backDelay: 2000,
+// 	});
+// }
